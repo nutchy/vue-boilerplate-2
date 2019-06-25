ea5d973c52204d2cfc3235060aa4802dc586c0e2
Chayanon Thongpila <58070021@kmitl.ac.th>
2019-06-25 23:53:20 +0700

    Initial Vuex using typescript

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex , { StoreOptions } from 'vuex'
+import { RootState } from './types'
+import { profile } from './profile/index'
+
+// Make vue aware of Vuex
+Vue.use(Vuex)
+
+const store: StoreOptions<RootState> = {
+  state: {
+    version: '1.0.0', // a simple property
+  },
+  modules: {
+    profile,
+  },
+}
+
+export default new Vuex.Store<RootState>(store)
